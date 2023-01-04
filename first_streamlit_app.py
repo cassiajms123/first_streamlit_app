3abf16f92ebcee01d4d55a318cb68108a2c78e40
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-04 09:55:04 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.title('🍌🥭 Build Your Own Fruit Smoothie🍌🥭')
+streamlit.title('🍌🥭 Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
