17773ec5cbbb9526966ebb6b90b2f0707530ab02
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-04 10:28:39 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -3,6 +3,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
+
 streamlit.title('My Parents New Healthy Dinner!') # adding an exlamation mark
 
 streamlit.header('Breakfast Menu') 
@@ -13,7 +15,9 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.title('🍌🥭 Build Your Own Fruit Smoothie')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberies'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
