6f234ef725dc67d8f4d85e431ba95e9a77b75f66
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-04 09:44:28 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner!') # adding an exlamation mark
 
 streamlit.header('Breafast Menu') 
-steramlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
