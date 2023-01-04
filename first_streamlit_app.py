998cbc028facf3c8881f8b59f9c8b35c2795ba6f
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-04 09:53:19 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Dinner!') # adding an exlamation mark
 
@@ -6,4 +7,8 @@ streamlit.header('Breafast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
 streamlit.text('🍌🥭 Build Your Own Fruit Smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
