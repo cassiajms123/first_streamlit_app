ebaf838c00f9df1a186cd7a8b5609cee573cd890
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-16 13:26:10 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
-# import pandas
-# import requests
-# import snowflake.connector
+import pandas
+import requests
+import snowflake.connector
 from urllib.error import URLError
-streamlit.stop()
+# streamlit.stop()
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
