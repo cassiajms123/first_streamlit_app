266e282d3ba3ddb0f15c31840e2e6db39424522b
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-16 15:28:32 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -19,3 +19,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+# Creating USER INTERACTION 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+streamlit.dataframe(my_fruit_list)
+
+
