8013b18615448aba7996fbfc3c88f8a6eb9c6722
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-16 15:07:03 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -8,9 +8,8 @@ from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.title('My Parents New Healthy Dinner!') 
 
-
-streamlit.title('My Parents New Healthy Dinner!') # adding an exlamation mark
 
 streamlit.header('Breakfast Menu') 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
