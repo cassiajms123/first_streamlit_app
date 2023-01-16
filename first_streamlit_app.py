71ed2996520d3dcb046e4c3149d1d8b6eb00a963
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-16 15:31:23 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -20,7 +20,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 # Creating USER INTERACTION 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
-streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
