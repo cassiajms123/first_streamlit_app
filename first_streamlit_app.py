0e99ea6349982045766ef24bf272896f2dc4f481
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-16 13:08:10 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -55,4 +55,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+# add a second text Entry Box
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
