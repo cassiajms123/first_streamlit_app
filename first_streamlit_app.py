4eb047a9b87a18464ac9457662bded1464eb45b6
cassiajms123 <121933842+cassiajms123@users.noreply.github.com>
2023-01-17 17:41:32 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -15,6 +15,12 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# IMPORTING DATA FROM A CSV PLACE IN AWS
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+# Customer choose fruits by name 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Creating USER INTERACTION 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page
